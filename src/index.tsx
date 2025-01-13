d1d6ac90b84bc7fd9f36535f3ed79117cb3bc89d
hckang80 <hckang80@gmail.com>
2025-01-13 22:21:13 +0900

    feat: inject react-query

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,19 @@ import { render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 const rootElement = document.getElementById('root');
-render(<App />, rootElement);
+render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={true} />
+    <App />
+  </QueryClientProvider>,
+  rootElement
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

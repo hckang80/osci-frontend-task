c077a470000f59f604b736b69d10f7b2fa3e2d70
hckang80 <hckang80@gmail.com>
2025-01-13 17:15:33 +0900

    feat: 페이지 라우터 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Users } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router
+      future={{
+        v7_relativeSplatPath: true,
+        v7_startTransition: true
+      }}
+    >
+      <Routes>
+        <Route path="/" element={<Navigate to="/users" replace />}></Route>
+        <Route path="/users" element={<Users />}></Route>
+      </Routes>
+    </Router>
   );
 }
 

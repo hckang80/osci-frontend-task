3f9f71e31caa1698c249aa751fafc610939e48ae
hckang80 <hckang80@gmail.com>
2025-01-13 21:55:29 +0900

    feat: 실제 데이터로 바인딩

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,20 +1,96 @@
-// import React from 'react';
-import React, { useEffect } from 'react';
+/**
+ * @jsxRuntime classic
+ * @jsx jsx
+ */
+import React, { FC, ReactNode, useEffect, useState } from 'react';
+import Avatar from '@atlaskit/avatar';
+import Link from '@atlaskit/link';
 
-// export const Users = () => {
-//   useEffect(() => {
-//     fetch(`${process.env.REACT_APP_BASE_URL}/users`)
-//       .then((response) => response.json())
-//       .then((data) => console.log(data));
-//   }, []);
-
-//   return <>사용자 목록</>;
-// };
+import { css, jsx } from '@emotion/react';
+import { Box, xcss } from '@atlaskit/primitives';
 
 import DynamicTable from '@atlaskit/dynamic-table';
-import { head, rows } from './content/sample-data';
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+const nameWrapperStyles = css({
+  display: 'flex',
+  alignItems: 'center'
+});
+
+const NameWrapper: FC<{ children: ReactNode }> = ({ children }) => (
+  <span css={nameWrapperStyles}>{children}</span>
+);
+
+const avatarWrapperStyles = xcss({
+  marginInlineEnd: 'space.100'
+});
+
+const AvatarWrapper: FC<{ children: ReactNode }> = ({ children }) => (
+  <Box xcss={avatarWrapperStyles}>{children}</Box>
+);
+
+const head = {
+  cells: [
+    {
+      key: 'name',
+      content: 'Name',
+      width: 25
+    },
+    {
+      key: 'party',
+      content: 'Party',
+      shouldTruncate: true,
+      width: 15
+    },
+    {
+      key: 'term',
+      content: 'Term',
+      shouldTruncate: true
+    }
+  ]
+};
 
 export const Users = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_BASE_URL}/users`)
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const rows = users.map((user) => ({
+    key: user.id + '',
+    isHighlighted: false,
+    cells: [
+      {
+        key: 'name',
+        content: (
+          <NameWrapper>
+            <AvatarWrapper>
+              <Avatar name={user.name} size="medium" />
+            </AvatarWrapper>
+            <Link href="https://atlassian.design">{user.name}</Link>
+          </NameWrapper>
+        )
+      },
+      {
+        key: 'email',
+        content: user.email
+      },
+      {
+        key: 'phone',
+        content: user.phone
+      }
+    ]
+  }));
+
   return (
     <DynamicTable
       caption="List of US Presidents"

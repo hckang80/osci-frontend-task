ad56777214374411198e179832bb18e61c67d1d0
hckang80 <hckang80@gmail.com>
2025-01-13 17:42:15 +0900

    feat: 목업 데이터로 유저 목록 구성

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,11 +1,32 @@
+// import React from 'react';
 import React, { useEffect } from 'react';
 
-export const Users = () => {
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_BASE_URL}/users`)
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }, []);
+// export const Users = () => {
+//   useEffect(() => {
+//     fetch(`${process.env.REACT_APP_BASE_URL}/users`)
+//       .then((response) => response.json())
+//       .then((data) => console.log(data));
+//   }, []);
 
-  return <>사용자 목록</>;
+//   return <>사용자 목록</>;
+// };
+
+import DynamicTable from '@atlaskit/dynamic-table';
+import { head, rows } from './content/sample-data';
+
+export const Users = () => {
+  return (
+    <DynamicTable
+      caption="List of US Presidents"
+      head={head}
+      rows={rows}
+      rowsPerPage={5}
+      defaultPage={1}
+      isFixedSize
+      defaultSortKey="term"
+      defaultSortOrder="ASC"
+      onSort={() => console.log('onSort')}
+      onSetPage={() => console.log('onSetPage')}
+    />
+  );
 };

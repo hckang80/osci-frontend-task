27fbaff7be861cee46afd0fefe48260cb5f54e12
hckang80 <hckang80@gmail.com>
2025-01-14 18:49:18 +0900

    feat: fetch 매개변수 보완

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
-export function fetcher<T>(url: string | URL | Request, init?: RequestInit): Promise<T> {
-  return fetch(`${process.env.REACT_APP_BASE_URL}${url}`).then((res) => res.json());
+export function fetcher<T>(
+  url: string | URL | Request,
+  init: RequestInit = {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+): Promise<T> {
+  return fetch(`${process.env.REACT_APP_BASE_URL}${url}`, init).then((res) => res.json());
 }

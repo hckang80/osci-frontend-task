0357fdf098343a80247a6f5f048bd5d8b4a23919
hckang80 <hckang80@gmail.com>
2025-01-14 23:53:46 +0900

    feat: 언어 설정 버튼 동적 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
 import './App.css';
-import 'i18n/config';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { Users, User, Posts } from './pages';
+import { useTranslation } from 'react-i18next';
+import { resources } from 'i18n/config';
 
 function App() {
+  const { i18n } = useTranslation();
+
   return (
-    <Router
-      future={{
-        v7_relativeSplatPath: true,
-        v7_startTransition: true
-      }}
-    >
-      <Routes>
-        <Route path="/" element={<Navigate to="/users" replace />}></Route>
-        <Route path="/users" element={<Users />}></Route>
-        <Route path="/users/:id" element={<User />}></Route>
-        <Route path="/posts" element={<Posts />}></Route>
-      </Routes>
-    </Router>
+    <>
+      {Object.keys(resources).map((lang) => (
+        <button key={lang} onClick={() => i18n.changeLanguage(lang)}>
+          {lang}
+        </button>
+      ))}
+      <Router
+        future={{
+          v7_relativeSplatPath: true,
+          v7_startTransition: true
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Navigate to="/users" replace />}></Route>
+          <Route path="/users" element={<Users />}></Route>
+          <Route path="/users/:id" element={<User />}></Route>
+          <Route path="/posts" element={<Posts />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -3,16 +3,18 @@ import { initReactI18next } from 'react-i18next';
 import translation from './en/translation.json';
 import translationKo from './ko/translation.json';
 
+export const resources = {
+  en: {
+    translation
+  },
+  ko: {
+    translation: translationKo
+  }
+};
+
 i18next.use(initReactI18next).init({
   lng: navigator.language,
   fallbackLng: 'en',
   debug: true,
-  resources: {
-    en: {
-      translation
-    },
-    ko: {
-      translation: translationKo
-    }
-  }
+  resources
 });

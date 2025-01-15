893a158b0b4e69c0632e5e2bed8f02fe55ed82ba
hckang80 <hckang80@gmail.com>
2025-01-15 10:49:22 +0900

    feat: 정보 수정 템플릿 적용

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { InlineEditableTextfield } from '@atlaskit/inline-edit';
 
 export const User = () => {
   const { id = '' } = useParams();
+  const placeholderLabel = 'Initial description value';
+  const [editValue, setEditValue] = useState('Default description value');
 
-  return <div>{`User ID: ${id}`}</div>;
+  const validate = (value: string) => {
+    if (value.length <= 6) {
+      return 'Please enter a description longer than 6 characters';
+    }
+    return undefined;
+  };
+
+  return (
+    <>
+      <div>{`User ID: ${id}`}</div>
+      <InlineEditableTextfield
+        defaultValue={editValue}
+        label="Description"
+        editButtonLabel={editValue || placeholderLabel}
+        onConfirm={(value) => setEditValue(value)}
+        placeholder={placeholderLabel}
+        validate={validate}
+      />
+    </>
+  );
 };

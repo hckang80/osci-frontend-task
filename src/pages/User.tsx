26a47aa621e2fcbe693de78484c18d8bdaab3e90
hckang80 <hckang80@gmail.com>
2025-01-15 12:08:26 +0900

    feat: 수정 UI를 form 기반으로 변경

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { InlineEditableTextfield } from '@atlaskit/inline-edit';
+import Form, { Field, FormFooter } from '@atlaskit/form';
+import Textfield from '@atlaskit/textfield';
+import ButtonGroup from '@atlaskit/button/button-group';
+import Button from '@atlaskit/button/new';
 import Spinner from '@atlaskit/spinner';
 import { fetcher } from 'lib/utils';
 import { useQuery } from 'react-query';
@@ -19,7 +22,6 @@ export const UserPage = () => {
     retry: 0,
     onSuccess: (data) => {
       setUser(data);
-      setEditValue(data.name);
     },
     onError: (error) => {
       if (!(error instanceof Error)) return;
@@ -27,15 +29,16 @@ export const UserPage = () => {
     }
   });
 
-  const placeholderLabel = 'Initial description value';
-  const [editValue, setEditValue] = useState('');
-
   if ((isError || !data) && error instanceof Error) {
     return <span>Error: {error.message}</span>;
   }
 
-  const validate = (value: string) => {
-    return !value.length ? 'Please enter your name' : undefined;
+  const handleSubmit = async (formState: Omit<User, 'id'>) => {
+    const data = await fetcher<User>(`/users/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(formState)
+    });
+    console.log({ data });
   };
 
   return (
@@ -43,15 +46,26 @@ export const UserPage = () => {
       {!user ? (
         <Spinner />
       ) : (
-        <>
-          <InlineEditableTextfield
-            defaultValue={editValue}
-            label={t('label.name')}
-            onConfirm={(value) => setEditValue(value)}
-            placeholder={placeholderLabel}
-            validate={validate}
-          />
-        </>
+        <Form onSubmit={handleSubmit}>
+          {({ formProps }) => (
+            <form {...formProps}>
+              <Field name="name" label={t('label.name')} defaultValue={user.name} isRequired>
+                {({ fieldProps }) => <Textfield {...fieldProps} />}
+              </Field>
+              <Field name="email" label={t('label.email')} defaultValue={user.email} isRequired>
+                {({ fieldProps }) => <Textfield {...fieldProps} />}
+              </Field>
+              <FormFooter>
+                <ButtonGroup>
+                  <Button appearance="subtle">Cancel</Button>
+                  <Button type="submit" appearance="primary" isLoading={false}>
+                    Edit
+                  </Button>
+                </ButtonGroup>
+              </FormFooter>
+            </form>
+          )}
+        </Form>
       )}
     </>
   );

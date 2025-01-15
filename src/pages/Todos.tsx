5f236d5a25e05ff00f802794193decbfbe79e5f3
hckang80 <hckang80@gmail.com>
2025-01-15 22:16:08 +0900

    feat: 유저별 todos 출력하기

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,8 +1,19 @@
+import { fetcher } from 'lib/utils';
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 export const TodosPage = () => {
   const { id = '' } = useParams();
 
-  return <span>{id}</span>;
+  const { data: todos } = useQuery('todos', () => fetcher(`/todos/user/${id}`), {
+    onSuccess: (data) => {
+      console.log(data);
+    },
+    onError: (error) => {
+      console.error(error);
+    }
+  });
+
+  return <pre>{JSON.stringify(todos, null, 2)}</pre>;
 };

a6d526f921b6d853cf5a5ce59b41fbd888c8fffc
hckang80 <hckang80@gmail.com>
2025-01-15 17:33:52 +0900

    feat: 게시물 표현에 필요한 요소만 남기기

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import Form, { ErrorMessage, Field, FormFooter } from '@atlaskit/form';
-import Textfield from '@atlaskit/textfield';
-import ButtonGroup from '@atlaskit/button/button-group';
-import Button from '@atlaskit/button/new';
 import Spinner from '@atlaskit/spinner';
 import { fetcher } from 'lib/utils';
-import { useMutation, useQuery } from 'react-query';
-import { t } from 'i18next';
-import { User } from 'lib/types';
-
-type UserForm = Omit<User, 'id'>;
+import { useQuery } from 'react-query';
+import { Post } from 'lib/types';
 
 export const PostPage = () => {
   const { id = '' } = useParams();
 
-  const fetchUser = () => fetcher<User>(`/users/${id}`);
+  const fetchPost = () => fetcher<Post>(`/posts/${id}`);
 
   const {
     isLoading,
     isError,
-    data: user,
+    data: post,
     error
-  } = useQuery<User>('users', fetchUser, {
+  } = useQuery<Post>('users', fetchPost, {
     refetchOnWindowFocus: false,
     retry: 0,
     onSuccess: (data) => {
@@ -33,43 +26,6 @@ export const PostPage = () => {
     }
   });
 
-  const updateUser = (formState: UserForm) => {
-    return fetcher<User>(`/users/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify(formState)
-    });
-  };
-
-  const updateUserMutation = useMutation(updateUser, {
-    onMutate: (variable) => {
-      console.log('onMutate', variable);
-    },
-    onError: (error, variable, context) => {
-      console.error(error);
-    },
-    onSuccess: (data, variables, context) => {
-      console.log('success', data, variables, context);
-    },
-    onSettled: () => {
-      console.log('end');
-    }
-  });
-
-  const { isLoading: isUpdating } = updateUserMutation;
-
-  const handleSubmit = (formState: UserForm) => {
-    const errors = {
-      email: !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(formState.email)
-        ? t('paragraph.validate.email')
-        : undefined
-    };
-
-    const isInvalid = Object.values(errors).some((value) => value !== undefined);
-    !isInvalid && updateUserMutation.mutate(formState);
-
-    return errors;
-  };
-
   const navigate = useNavigate();
 
   if (isError && error instanceof Error) {
@@ -78,36 +34,12 @@ export const PostPage = () => {
 
   return (
     <>
-      {isLoading || !user ? (
+      {isLoading || !post ? (
         <Spinner />
       ) : (
-        <Form onSubmit={handleSubmit}>
-          {({ formProps }) => (
-            <form {...formProps}>
-              <Field name="name" label={t('label.name')} defaultValue={user.name} isRequired>
-                {({ fieldProps }) => <Textfield {...fieldProps} isReadOnly={isUpdating} />}
-              </Field>
-              <Field name="email" label={t('label.email')} defaultValue={user.email} isRequired>
-                {({ fieldProps, error }) => (
-                  <>
-                    <Textfield {...fieldProps} isReadOnly={isUpdating} />
-                    {error && <ErrorMessage>{error}</ErrorMessage>}
-                  </>
-                )}
-              </Field>
-              <FormFooter>
-                <ButtonGroup>
-                  <Button appearance="subtle" onClick={() => navigate(-1)}>
-                    {t('label.cancel')}
-                  </Button>
-                  <Button type="submit" appearance="primary" isLoading={isUpdating}>
-                    {t('label.edit')}
-                  </Button>
-                </ButtonGroup>
-              </FormFooter>
-            </form>
-          )}
-        </Form>
+        <>
+          <pre>{JSON.stringify(post, null, 2)}</pre>
+        </>
       )}
     </>
   );

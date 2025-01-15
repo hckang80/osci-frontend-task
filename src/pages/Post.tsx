3db6f60ce7c67f25b2001e9eb4b5560e08c9ba0c
hckang80 <hckang80@gmail.com>
2025-01-15 17:37:18 +0900

    feat: 댓글 목업 적용

--- src/pages/Post.tsx
+++ src/pages/Post.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Spinner from '@atlaskit/spinner';
+import Avatar from '@atlaskit/avatar';
+import Comment, { CommentAction, CommentAuthor, CommentTime } from '@atlaskit/comment';
 import { fetcher } from 'lib/utils';
 import { useQuery } from 'react-query';
 import { Post } from 'lib/types';
@@ -39,6 +41,42 @@ export const PostPage = () => {
       ) : (
         <>
           <pre>{JSON.stringify(post, null, 2)}</pre>
+          <Comment
+            avatar={<Avatar name="Scott Farquhar" />}
+            author={<CommentAuthor>Scott Farquhar</CommentAuthor>}
+            type="author"
+            time={<CommentTime>Jun 3, 2022</CommentTime>}
+            content={
+              <p>
+                Hard to believe it’s been 20 years since we started Atlassian, but we’re just
+                getting started!
+              </p>
+            }
+            actions={[
+              <CommentAction>Reply</CommentAction>,
+              <CommentAction>Edit</CommentAction>,
+              <CommentAction>Like</CommentAction>
+            ]}
+          >
+            <Comment
+              avatar={<Avatar name="John Smith" />}
+              author={<CommentAuthor>John Smith</CommentAuthor>}
+              time={<CommentTime>Jun 3, 2022</CommentTime>}
+              content={<p>Congratulations!</p>}
+              actions={[<CommentAction>Reply</CommentAction>, <CommentAction>Like</CommentAction>]}
+            >
+              <Comment
+                avatar={<Avatar name="Sabina Vu" />}
+                author={<CommentAuthor>Sabina Vu</CommentAuthor>}
+                time={<CommentTime>Jun 4, 2022</CommentTime>}
+                content={<p>I wonder what Atlassian will be like 20 years from now?</p>}
+                actions={[
+                  <CommentAction>Reply</CommentAction>,
+                  <CommentAction>Like</CommentAction>
+                ]}
+              />
+            </Comment>
+          </Comment>
         </>
       )}
     </>

0d6bb85e536695ffb029ca474ab1f1732899980a
hckang80 <hckang80@gmail.com>
2025-01-16 12:27:21 +0900

    feat: 목업 피커 적용

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -6,7 +6,9 @@ import React, { Fragment, useMemo, useState } from 'react';
 
 import { jsx } from '@emotion/react';
 
-import Form, { Field } from '@atlaskit/form';
+import Form, { Field, Label } from '@atlaskit/form';
+import { DatePicker } from '@atlaskit/datetime-picker';
+import { parseISO } from 'date-fns';
 import Textfield from '@atlaskit/textfield';
 import DynamicTable from '@atlaskit/dynamic-table';
 import SearchIcon from '@atlaskit/icon/core/migration/search';
@@ -123,6 +125,20 @@ export const PostsPage = () => {
 
   return (
     <Fragment>
+      <Label id="custom" htmlFor="datepicker-format">
+        Custom Date Format
+      </Label>
+      <DatePicker
+        dateFormat="YYYY-MM-DD"
+        placeholder="2021-06-10"
+        parseInputValue={(date: string) => parseISO(date)}
+        id="datepicker-format"
+        clearControlLabel="Clear Custom Date Format"
+        shouldShowCalendarButton
+        inputLabelId="custom"
+        openCalendarLabel="open calendar"
+      />
+
       <Form onSubmit={handleSubmit}>
         {({ formProps }) => (
           <form {...formProps} name="validation-example">
